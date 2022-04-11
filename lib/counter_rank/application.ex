ac696756c5f4171bb0748ffa0f246b14ba5e838f
Kamil Lelonek <squixy.sln@gmail.com>
2022-04-11 09:32:32 +0200

    Start the worker in Application

--- lib/counter_rank/application.ex
+++ lib/counter_rank/application.ex
@@ -3,11 +3,29 @@ defmodule CounterRank.Application do
 
   use Application
 
+  require Logger
+
   @impl true
   def start(_type, _args), do: Supervisor.start_link(children(), opts())
 
   defp children do
-    []
+    [
+      {
+        CounterRank.Worker,
+        [defaul_counter_value()]
+      }
+    ]
+  end
+
+  defp defaul_counter_value do
+    case Application.fetch_env(:counter_rank, :defaul_counter_value) do
+      {:ok, defaul_counter_value} ->
+        defaul_counter_value
+
+      :error ->
+        Logger.warn("No value for `defaul_counter_value` found in config!")
+        nil
+    end
   end
 
   defp opts do
